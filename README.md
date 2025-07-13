44e4be3dedaf2f78a0a22b994182be5ee470301c
Cayuã <cayuaronald7@gmail.com>
2025-07-13 17:38:47 -0400

    Initial commit of IMC calculator project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Calculadora de IMC 
+
+## Descrição
+Esta é uma aplicação simples para calcular o Índice de Massa Corporal (IMC) usando HTML, CSS e JavaScript. O IMC é uma medida utilizada para avaliar se uma pessoa está dentro de um peso saudável em relação à sua altura.
+
+## Objetivo
+Este projeto teve como objetivo praticar habilidades em manipulação do DOM, responsividade e interação com o usuário. Foram utilizados HTML para estruturar a página, CSS para estilização e JavaScript para implementar a lógica de cálculo do IMC e controlar os elementos da interface.
+
+## Tecnologias Utilizadas
+- HTML  
+- CSS
+- JavaScript
+- Manipulação do DOM
+- Responsividade com Media Queries
+
+## Funcionalidades
+- Entrada de peso e altura pelo usuário.
+- Cálculo do IMC com base nos valores inseridos.
+- Exibição do resultado do IMC e uma mensagem interpretativa.
+- Responsividade para diferentes tamanhos de tela.
+
+## Como Usar
+1. Abra o arquivo `index.html` em um navegador web.
+2. Insira seu peso em quilogramas e sua altura em metros.   
+3. Clique no botão "Calcular IMC".
+4. O resultado do IMC será exibido junto com uma mensagem interpretativa.
+
+🔗 [Acesse aqui a calculadora online](https://cayua.github.io/imc-dom/)
---
+++ imc.js
@@ -0,0 +1,55 @@
+document.getElementById("btn-send").addEventListener("click", (event) => { 
+
+    event.preventDefault(); // Prevent form submission
+
+    const weight  = parseFloat(document.getElementById("peso").value);
+    const height = parseFloat(document.getElementById("altura").value);
+    const result = document.getElementById("result");
+    const msgImc = document.getElementById("msg-imc");
+    let imc = 0;
+
+    if(checkNumber(weight, height)){
+        imc = calculateImc(weight, height).toFixed(1);
+        result.textContent = "Seu IMC é: " + imc;
+        msgImc.textContent = "Classificação: " + returnMsg(imc);
+    }
+    else {
+        result.textContent = "Por favor, digite um valor válido!"
+        msgImc.textContent = "";
+    }
+
+})
+
+function checkNumber(value1, value2) {
+    return value1 > 0 && value2 > 0
+}
+
+function calculateImc(weight, height){
+     return weight / (height * height)
+}
+
+function returnMsg(imc){
+    if(imc < 18.5){
+        return "abaixo do peso";
+    } else if(imc >= 18.5 && imc < 24.9){
+        return "peso normal";
+    } else if(imc >= 25 && imc < 29.9){
+        return "sobrepeso";
+    } else if(imc >= 30 && imc < 34.9){
+        return "obesidade grau 1";
+    } else if(imc >= 35 && imc < 39.9){
+        return "obesidade grau 2";
+    } else {
+        return "obesidade grau 3";
+    }
+}
+
+document.getElementById("btn-clean").addEventListener("click", () => {
+    const result = document.getElementById("result");
+    const msgImc = document.getElementById("msg-imc")
+
+    result.textContent = "";
+    msgImc.textContent = "";
+})
+
+
