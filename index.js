2ef06481b00ae3df88c1aeea6f0a6a0f4cc72d54
Cayuã <cayuaronald7@gmail.com>
2025-09-03 19:54:56 -0400

    feat: add callback functions for validation and display

--- index.js
+++ index.js
@@ -1,53 +1,101 @@
+const resultElement = document.getElementById("result");
+const msgElement = document.getElementById("msg-imc")
+
+function displayIMC(imc, msgIMC) {
+    resultElement.textContent = "Seu IMC é: " + imc;
+    msgElement.textContent = "Classificação: " + msgIMC;
+}
+
+function classificationIMC(imc){
+    if(imc < 16){
+        return "Extremamente abaixo do peso";
+    } else if(imc < 16.9){
+        return "Muito abaixo do peso";
+    } else if(imc < 18.4){
+        return "Abaixo do peso";
+    } else if(imc < 24.9){
+        return "Peso normal";
+    } else if(imc < 29.9){
+        return "Acima do peso";
+    } else if(imc < 34.9){
+        return "Obesidade grau 1";
+    } else if(imc < 40){
+        return "Obesidade grau 2";
+    } else {
+        return "Obesidade grau 3";
+    }
+}
+
+const calculateIMC = (weight, height) => weight / (height * height);
+
+function checkNumber (value1, value2) {
+    
+    if(value1 < 0 && value2 < 0){
+        return false;
+    } else if(value1 < value2){
+        return false;
+    } else if(value1 == null || value2 == null){
+        return false
+    } else if(Number.isNaN(value1) || Number.isNaN(value2)){
+        return false
+    } else {
+        return true;
+    }
+} 
+
+function execute(check, calculate, classification, display) {
+
+        const weight  = parseFloat(document.getElementById("weight-element").value);
+        const height = parseFloat(document.getElementById("height-element").value);
+
+        if(check(weight, height)){
+            const imc = calculate(weight, height).toFixed(1);
+            const msgIMC = classification(imc)
+            display(imc, msgIMC)        
+        }
+
+        else {
+            throw new Error("Os valores de peso e altura não podem estar vazios e precisam ser maiores que zero, além disso a altura não pode ser maior que o peso!");        
+        }
+    
+}
+
+function checkParams (arrFunctions) {
+
+    if(arrFunctions.length < 5){
+        throw new Error("Quantidade de parâmetros inválida");  
+    }
+
+    for(fn of arrFunctions){
+        if(typeof fn !== "function"){
+            throw new Error("Parâmetros inválidos");   
+        } 
+    }
+    return true;
+}
+
 document.getElementById("btn-send").addEventListener("click", () => { 
 
-    const weight  = parseFloat(document.getElementById("peso").value);
-    const height = parseFloat(document.getElementById("altura").value);
-    const result = document.getElementById("result");
-    const msgImc = document.getElementById("msg-imc");
-    let imc = 0;
-
-    if(checkNumber(weight, height)){
-        imc = calculateImc(weight, height).toFixed(1);
-        result.textContent = "Seu IMC é: " + imc;
-        msgImc.textContent = "Classificação: " + returnMsg(imc);
-    }
-    else {
-        result.textContent = "Por favor, digite um valor válido!"
-        msgImc.textContent = "";
-    }
+        try {
+            checkParams( [execute, checkNumber, calculateIMC, classificationIMC, displayIMC] ) 
+            execute(checkNumber, calculateIMC, classificationIMC, displayIMC)
+        } catch (error) {
+            msgElement.textContent = error.message;
+        }  
 
 })
-
-function checkNumber(value1, value2) {
-    return value1 > 0 && value2 > 0
-}
-
-function calculateImc(weight, height){
-     return weight / (height * height)
-}
-
-function returnMsg(imc){
-    if(imc < 18.5){
-        return "abaixo do peso";
-    } else if(imc >= 18.5 && imc < 24.9){
-        return "peso normal";
-    } else if(imc >= 25 && imc < 29.9){
-        return "sobrepeso";
-    } else if(imc >= 30 && imc < 34.9){
-        return "obesidade grau 1";
-    } else if(imc >= 35 && imc < 39.9){
-        return "obesidade grau 2";
-    } else {
-        return "obesidade grau 3";
-    }
-}
 
 document.getElementById("btn-clean").addEventListener("click", () => {
-    const result = document.getElementById("result");
-    const msgImc = document.getElementById("msg-imc")
-
-    result.textContent = "";
-    msgImc.textContent = "";
+    resultElement.textContent = "";
+    msgElement.textContent = "";
 })
+
+
+
+
+
+
+
+
 
 
