cec224a50d20f3e3cf04d318b82f634918338d97
Cayuã <cayuaronald7@gmail.com>
2025-09-03 21:02:45 -0400

    refactor: improve functions and add code comments

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+//Variáveis que contém os elementos HTML que vão exibir as mensagem de resultado
 const resultElement = document.getElementById("result");
 const msgElement = document.getElementById("msg-imc")
 
+//Variáveis que contém os botões de calcular e limpar
+const btnCalc = document.getElementById("btn-send")
+const btnClean = document.getElementById("btn-clean")
+
+
+//Função que imprime o IMC do usuário e a sua classificação
 function displayIMC(imc, msgIMC) {
     resultElement.textContent = "Seu IMC é: " + imc;
     msgElement.textContent = "Classificação: " + msgIMC;
 }
 
+//Função que retorna uma mensagem de classificação de IMC
 function classificationIMC(imc){
     if(imc < 16){
         return "Extremamente abaixo do peso";
@@ -26,58 +34,39 @@ function classificationIMC(imc){
     }
 }
 
+//Função que calcula o IMC do usuário
 const calculateIMC = (weight, height) => weight / (height * height);
 
+//Função que faz uma verificação nos valores recebidos pelos inputs
 function checkNumber (value1, value2) {
     
-    if(value1 < 0 && value2 < 0){
-        return false;
-    } else if(value1 < value2){
-        return false;
-    } else if(value1 == null || value2 == null){
-        return false
+    if(value1 <= 0 || value2 <= 0){
+        throw new Error("Os valores de peso e altura precisam ser maiores que zero");
     } else if(Number.isNaN(value1) || Number.isNaN(value2)){
-        return false
+        throw new Error("Os valores de peso e altura precisam ser números");
     } else {
         return true;
     }
 } 
 
+//Função principal que executa toda a lógica da aplicação
 function execute(check, calculate, classification, display) {
 
         const weight  = parseFloat(document.getElementById("weight-element").value);
         const height = parseFloat(document.getElementById("height-element").value);
 
-        if(check(weight, height)){
-            const imc = calculate(weight, height).toFixed(1);
-            const msgIMC = classification(imc)
-            display(imc, msgIMC)        
-        }
+        check(weight, height)
 
-        else {
-            throw new Error("Os valores de peso e altura não podem estar vazios e precisam ser maiores que zero, além disso a altura não pode ser maior que o peso!");        
-        }
+        const imc = calculate(weight, height).toFixed(1);
+        const msgIMC = classification(imc)
+        display(imc, msgIMC)        
     
 }
 
-function checkParams (arrFunctions) {
-
-    if(arrFunctions.length < 5){
-        throw new Error("Quantidade de parâmetros inválida");  
-    }
-
-    for(fn of arrFunctions){
-        if(typeof fn !== "function"){
-            throw new Error("Parâmetros inválidos");   
-        } 
-    }
-    return true;
-}
-
-document.getElementById("btn-send").addEventListener("click", () => { 
+//Função com evento de clique que chama a função execute toda vez que o botão é clicado
+btnCalc.addEventListener("click", () => { 
 
         try {
-            checkParams( [execute, checkNumber, calculateIMC, classificationIMC, displayIMC] ) 
             execute(checkNumber, calculateIMC, classificationIMC, displayIMC)
         } catch (error) {
             msgElement.textContent = error.message;
@@ -85,7 +74,8 @@ document.getElementById("btn-send").addEventListener("click", () => {
 
 })
 
-document.getElementById("btn-clean").addEventListener("click", () => {
+//Função com evento de clique que limpa os resultados da tela
+btnClean.addEventListener("click", () => {
     resultElement.textContent = "";
     msgElement.textContent = "";
 })
