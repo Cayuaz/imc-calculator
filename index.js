d0c1d4f014a66322bf60367cc5e780a6f6ff5718
Cayuã <cayuaronald7@gmail.com>
2025-09-03 22:51:35 -0400

    refactor: add import and export modules

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
+import {displayIMC, classificationIMC, calculateIMC, checkNumber} from './logic.js'
+
 //Variáveis que contém os elementos HTML que vão exibir as mensagem de resultado
 const resultElement = document.getElementById("result");
-const msgElement = document.getElementById("msg-imc")
+const msgElement = document.getElementById("msg-imc");
 
 //Variáveis que contém os botões de calcular e limpar
 const btnCalc = document.getElementById("btn-send")
 const btnClean = document.getElementById("btn-clean")
 
 
-//Função que imprime o IMC do usuário e a sua classificação
-function displayIMC(imc, msgIMC) {
-    resultElement.textContent = "Seu IMC é: " + imc;
-    msgElement.textContent = "Classificação: " + msgIMC;
-}
-
-//Função que retorna uma mensagem de classificação de IMC
-function classificationIMC(imc){
-    if(imc < 16){
-        return "Extremamente abaixo do peso";
-    } else if(imc < 16.9){
-        return "Muito abaixo do peso";
-    } else if(imc < 18.4){
-        return "Abaixo do peso";
-    } else if(imc < 24.9){
-        return "Peso normal";
-    } else if(imc < 29.9){
-        return "Acima do peso";
-    } else if(imc < 34.9){
-        return "Obesidade grau 1";
-    } else if(imc < 40){
-        return "Obesidade grau 2";
-    } else {
-        return "Obesidade grau 3";
-    }
-}
-
-//Função que calcula o IMC do usuário
-const calculateIMC = (weight, height) => weight / (height * height);
-
-//Função que faz uma verificação nos valores recebidos pelos inputs
-function checkNumber (value1, value2) {
-    
-    if(value1 <= 0 || value2 <= 0){
-        throw new Error("Os valores de peso e altura precisam ser maiores que zero");
-    } else if(Number.isNaN(value1) || Number.isNaN(value2)){
-        throw new Error("Os valores de peso e altura precisam ser números");
-    } else {
-        return true;
-    }
-} 
 
 //Função principal que executa toda a lógica da aplicação
 function execute(check, calculate, classification, display) {
@@ -59,7 +20,7 @@ function execute(check, calculate, classification, display) {
 
         const imc = calculate(weight, height).toFixed(1);
         const msgIMC = classification(imc)
-        display(imc, msgIMC)        
+        display(imc, msgIMC, resultElement, msgElement)        
     
 }
 
@@ -69,6 +30,7 @@ btnCalc.addEventListener("click", () => {
         try {
             execute(checkNumber, calculateIMC, classificationIMC, displayIMC)
         } catch (error) {
+            resultElement.textContent = "";
             msgElement.textContent = error.message;
         }  
 
---
+++ logic.js
@@ -0,0 +1,43 @@
+//Função que imprime o IMC do usuário e a sua classificação
+function displayIMC(imc, msgIMC, result, msgElement) {
+    result.textContent = "Seu IMC é: " + imc;
+    msgElement.textContent = "Classificação: " + msgIMC;
+}
+
+//Função que retorna uma mensagem de classificação de IMC
+function classificationIMC(imc){
+    if(imc < 16){
+        return "Extremamente abaixo do peso";
+    } else if(imc < 16.9){
+        return "Muito abaixo do peso";
+    } else if(imc < 18.4){
+        return "Abaixo do peso";
+    } else if(imc < 24.9){
+        return "Peso normal";
+    } else if(imc < 29.9){
+        return "Acima do peso";
+    } else if(imc < 34.9){
+        return "Obesidade grau 1";
+    } else if(imc < 40){
+        return "Obesidade grau 2";
+    } else {
+        return "Obesidade grau 3";
+    }
+}
+
+//Função que calcula o IMC do usuário
+const calculateIMC = (weight, height) => weight / (height * height);
+
+//Função que faz uma verificação nos valores recebidos pelos inputs
+function checkNumber (value1, value2) {
+    
+    if(value1 <= 0 || value2 <= 0){
+        throw new Error("Os valores de peso e altura precisam ser maiores que zero");
+    } else if(Number.isNaN(value1) || Number.isNaN(value2)){
+        throw new Error("Os valores de peso e altura precisam ser números");
+    } else {
+        return true;
+    }
+} 
+
+export {displayIMC, classificationIMC, calculateIMC, checkNumber}
